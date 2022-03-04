d4a6e12fd3b49d5351429b21a54168332a3a9b32
Bwilliams17 <29454@bridgend.ac.uk>
2022-03-04 13:27:18 +0000

    added direction

--- src/main.py
+++ src/main.py
@@ -8,6 +8,21 @@ def main():
     lives = 3
     print(f"You have {lives } Remaining lives")
 
+    print("directions avilible north,south,east,west")
+    TravelDirection=input("which direction would you like to go ?")
+
+    if TravelDirection == "north":
+            print("you went north")
+
+    elif TravelDirection == "south":
+            print("you went south")
+
+    elif TravelDirection == "east":
+            print("you when east")
+        
+    else:
+            print("you went west")
+
 
 
 
