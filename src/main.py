237810faa4191e5458a1a3441d4aa7f62682665e
S4ndm4n89 <90313903+S4ndm4n89@users.noreply.github.com>
2022-03-17 14:50:01 +0000

    Keys And backpack

--- src/main.py
+++ src/main.py
@@ -3,25 +3,44 @@ def main():
     print("Welcome to the Game")
 
     PlayerName=input("what is your name? ")
-    print("Hello " + PlayerName)
+    print(f"Hello " + PlayerName)
 
     lives = 3
     print(f"You have {lives } Remaining lives")
 
-    print("directions avilible north,south,east,west")
-    TravelDirection=input("which direction would you like to go ?")
+    backpack = [] # initialise empty list for backpack
 
-    if TravelDirection == "north":
-            print("you went north")
+    print("you awake in a room to your front is a Blue door, to your right is a Red door, to left is a Green door and behind you there is a Black Door. \nOn the floor in front of you, there is a note ! it read \n( welcome to My game, you need to complete four puzzles and collect there Key to escape. Be aware of hidden threats!) ") 
+    print("Upon looking around this central room again you find a backpack and put it on,\n you also notice door like shape in the corner of the room, it has four key like hole init.   ")  
 
-    elif TravelDirection == "south":
-            print("you went south")
+    while True:
+        direction = input("Which room do you want to try? please choice from Red, Blue, Green or Black. ")    
 
-    elif TravelDirection == "east":
-            print("you when east")
+        if direction == "Red":
+                print("you entered the Red Room. ")
+                puzzle_guess_Red = input("What is 2 + 2? ")
+                if puzzle_guess_Red == "4":
+                        print("correct")
+                        # We need to collect Something here - i.e add to Backpack
+                        backpack.append("key 1 collected")
+                else:
+                        print("Incorrect")
+                        lives -= 1
+                        print(f"You have {lives} lives remaining.")
+        elif direction == "Blue":
+                print("you entered the Blue Room. ")
+
+        elif direction == "Green":
+                print("you entered the Green Room. ")
         
-    else:
-            print("you went west")
+        elif direction == "Black": 
+                print("you entered the Black Room. ")
+        else:
+                print("Room not recognised!")   
+
+        #if back pack is full, open door and win game.
+        if ("Key 1" in backpack) and ("Key 2" in backpack) and ("Key 3" in backpack) and ("key 4" in backpack):
+                print(" you input the four keys into the strange door in the corner, it open and you see the outside, you run for freedom\n you escape! game over!!  ") 
 
 
 
